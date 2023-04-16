e9664c16689810dc187146776617dd743188010b
Brian Reardon <brian.d.reardon@gmail.com>
2023-04-16 11:29:29 -0400

    done

---
+++ lib/migrations/versions/e2306ca58fd3_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: e2306ca58fd3
+Revises: 
+Create Date: 2023-04-16 11:18:12.516229
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e2306ca58fd3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
